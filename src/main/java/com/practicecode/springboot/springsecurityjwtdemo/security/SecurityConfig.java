05526ff43f15e6ef81104ccb1aeeb2785762fe56
ravidugm <ravindumiyuranga@gmail.com>
2021-05-06 19:47:59 +0530

    Configure Spring security for permit to access authenticate endpoint

--- src/main/java/com/practicecode/springboot/springsecurityjwtdemo/security/SecurityConfig.java
+++ src/main/java/com/practicecode/springboot/springsecurityjwtdemo/security/SecurityConfig.java
@@ -2,7 +2,10 @@ package com.practicecode.springboot.springsecurityjwtdemo.security;
 
 import com.practicecode.springboot.springsecurityjwtdemo.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -15,5 +18,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(myUserDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
