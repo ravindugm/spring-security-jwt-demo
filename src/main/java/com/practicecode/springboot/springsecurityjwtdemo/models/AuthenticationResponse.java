3c7e6be5927ca83184296fe3631312bca1a88c52
ravidugm <ravindumiyuranga@gmail.com>
2021-05-06 14:03:13 +0530

    Add model classes

---
+++ src/main/java/com/practicecode/springboot/springsecurityjwtdemo/models/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.practicecode.springboot.springsecurityjwtdemo.models;
+
+public class AuthenticationResponse {
+
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
