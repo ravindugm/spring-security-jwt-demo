4353b413adf2e944a87dcad84ca0f52ff2f91721
ravidugm <ravindumiyuranga@gmail.com>
2021-05-06 23:11:27 +0530

    Add Jwt filter file

---
+++ src/main/java/com/practicecode/springboot/springsecurityjwtdemo/filters/JwtRequestFilter.java
@@ -0,0 +1,64 @@
+package com.practicecode.springboot.springsecurityjwtdemo.filters;
+
+import com.practicecode.springboot.springsecurityjwtdemo.service.MyUserDetailsService;
+import com.practicecode.springboot.springsecurityjwtdemo.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    // Get the Authorization header
+    // If authorizationHeader not null and its start with "Bearer " then gives jwt directly and using jwt extract the username
+    // If username is not null and user context doesn't already has authenticated user then pass that username and get userDetails
+    // Then validate token using validateToken() that verify username matching and jwt token is not expired
+    // UsernamePasswordAuthenticationToken - use for manage authentication context of username and password
+    // If token is not expired usernamePasswordAuthenticationToken set the details in the httpServletRequest
+    // Then continue the filter chain
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+
+        final String authorizationHeader = httpServletRequest.getHeader("Authorization");
+
+        String username = null;
+        String jwt = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
+            username = jwtUtil.extractUsername(jwt);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+            if (jwtUtil.validateToken(jwt, userDetails)) {
+
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
